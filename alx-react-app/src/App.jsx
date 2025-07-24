d527154784fdfc92750eef79716194b29c2a2f7e
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-07-24 19:57:39 +0100

    Add Header, MainContent, and Footer components and use them in App

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './componets/Header';
+
+import MainContent from './componets/MainContent';
+import Header from './componets/Footer';
+
+function App() {
+  return (
+    <><Footer />
+     <MainContent />
+       <Header />
+      
+    </>
+  );
+}
+
+export default App;
