e612fd6e8ab90a35359636d73f1243ffd4ca4911
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-21 20:17:24 +0100

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,13 @@
+import ProfilePage from './ProfilePage';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+return (
+    // غلف ProfilePage بـ UserContext.Provider
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );}
+
+export default App;
