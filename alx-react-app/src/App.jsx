fda5aea009a326e71312232efde0a6b185e89963
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-21 19:25:53 +0100

    Task 3: Added UserProfile component and used props in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,14 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
       <Header />
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <Footer />
     </>
