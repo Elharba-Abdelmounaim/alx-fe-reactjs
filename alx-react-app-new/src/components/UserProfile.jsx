0c0d3aa11edae5516284646c6114f49bec61a7ba
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-08-21 19:58:38 +0100

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile(props) {
+  return (
+    <div style={{ border: '1px solid gray', padding: '15px', margin: '10px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', color: 'darkred' }}>{props.age}</span></p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
